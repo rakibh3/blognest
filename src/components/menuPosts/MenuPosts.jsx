9a267410828fc02cec92ed9c91f9ba28a084b715
Md Rakibul Hasan <hello.rakibh@gmail.com>
2024-08-17 04:53:19 +0600

    Side item

--- src/components/menuPosts/MenuPosts.jsx
+++ src/components/menuPosts/MenuPosts.jsx
@@ -1,12 +1,12 @@
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
-import styles from "./menuPosts.module.css"
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import styles from './menuPosts.module.css';
 
 const MenuPosts = ({ withImage }) => {
   return (
     <div className={styles.items}>
-      <Link href="/" className={styles.item}>
+      <Link href="#" className={styles.item}>
         {withImage && (
           <div className={styles.imageContainer}>
             <Image src="/p1.jpeg" alt="" fill className={styles.image} />
@@ -15,15 +15,15 @@ const MenuPosts = ({ withImage }) => {
         <div className={styles.textContainer}>
           <span className={`${styles.category} ${styles.travel}`}>Travel</span>
           <h3 className={styles.postTitle}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Top 10 Hidden Gems in European Cities You Must Visit
           </h3>
           <div className={styles.detail}>
-            <span className={styles.username}>John Doe</span>
-            <span className={styles.date}> - 10.03.2023</span>
+            <span className={styles.username}>Hasan</span>
+            <span className={styles.date}> - 10.12.2023</span>
           </div>
         </div>
       </Link>
-      <Link href="/" className={styles.item}>
+      <Link href="#" className={styles.item}>
         {withImage && (
           <div className={styles.imageContainer}>
             <Image src="/p1.jpeg" alt="" fill className={styles.image} />
@@ -34,15 +34,15 @@ const MenuPosts = ({ withImage }) => {
             Culture
           </span>
           <h3 className={styles.postTitle}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            How to Travel Sustainably: Tips for Eco-Friendly Adventures
           </h3>
           <div className={styles.detail}>
-            <span className={styles.username}>John Doe</span>
-            <span className={styles.date}> - 10.03.2023</span>
+            <span className={styles.username}>Rakib</span>
+            <span className={styles.date}> - 10.10.2023</span>
           </div>
         </div>
       </Link>
-      <Link href="/" className={styles.item}>
+      <Link href="#" className={styles.item}>
         {withImage && (
           <div className={styles.imageContainer}>
             <Image src="/p1.jpeg" alt="" fill className={styles.image} />
@@ -51,15 +51,15 @@ const MenuPosts = ({ withImage }) => {
         <div className={styles.textContainer}>
           <span className={`${styles.category} ${styles.food}`}>Food</span>
           <h3 className={styles.postTitle}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            The Ultimate Guide to Digital Nomad Destinations in 2024
           </h3>
           <div className={styles.detail}>
-            <span className={styles.username}>John Doe</span>
-            <span className={styles.date}> - 10.03.2023</span>
+            <span className={styles.username}>Morsalin</span>
+            <span className={styles.date}> - 10.11.2023</span>
           </div>
         </div>
       </Link>
-      <Link href="/" className={styles.item}>
+      <Link href="#" className={styles.item}>
         {withImage && (
           <div className={styles.imageContainer}>
             <Image src="/p1.jpeg" alt="" fill className={styles.image} />
@@ -70,10 +70,10 @@ const MenuPosts = ({ withImage }) => {
             Fashion
           </span>
           <h3 className={styles.postTitle}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Essential Packing Tips for Stress-Free Travel
           </h3>
           <div className={styles.detail}>
-            <span className={styles.username}>John Doe</span>
+            <span className={styles.username}>Dr. Viku</span>
             <span className={styles.date}> - 10.03.2023</span>
           </div>
         </div>
