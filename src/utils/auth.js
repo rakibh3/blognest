a3f266683bc673ab1866808c638baeebfd967383
Md Rakibul Hasan <hello.rakibh@gmail.com>
2024-08-18 17:20:39 +0600

    Side item

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,8 +1,8 @@
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import GithubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
-import prisma from "./connect";
-import { getServerSession } from "next-auth";
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+import prisma from './connect';
+import { getServerSession } from 'next-auth';
 
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
@@ -10,10 +10,7 @@ export const authOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
-    }),
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      allowDangerousEmailAccountLinking: true,
     }),
   ],
 };
